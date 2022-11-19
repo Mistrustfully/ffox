112f93dd8384d6d854154e9ab71ac8df5fd79789
Mistrustfully <mistrustfully@gmail.com>
2022-11-18 21:44:24 -0600

    why is out unignored !!!

--- out/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const lexer: (source: string) => unknown[];
-export declare const parser: (tokens: unknown[]) => unknown;
-export declare const compiler: (ast: unknown) => { bytes: number[]; constants: unknown[] };
-export declare const vm: {
-	run: (bytes: number[], constants: unknown[]) => unknown;
-};
--- out/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-	compiler = require(script.frontend.compiler),
-	parser = require(script.frontend.parser),
-	lexer = require(script.frontend.lex),
-	vm = require(script.vm),
-}
